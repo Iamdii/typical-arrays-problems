bf0948128fba0befe0432ee4c0602f58f3d2f5e2
Di <azazarova99@mail.ru>
2020-03-05 00:24:11 +0300

    init: implemented task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+  var result = array[0];
+
+  for(var i = 1; i < array.length; i++){
+    if(array[i] < result) {
+      result = array[i];
+    }
+  }
+
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+  var result = array[0];
+
+  for(var i = 1; i < array.length; i++){
+    if(array[i] > result) {
+      result = array[i];
+    }
+  }
+
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+  var result = array[0];
+
+  for(var i = 1; i < array.length; i++){
+    result += array[i];
+  }
+
+  return result/array.length;
 }
